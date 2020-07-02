da59c8d650ac1c1ffaef7db10eadf8e4aed153eb
reiatsu86 <reiatsu86@gmail.com>
2020-07-02 16:00:07 -0600

    update the JS_1
    assignement step

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+window.alert("DOGS DOGS DOGS")      //My window alert
+
+var A = "Pigs are cute,";           //Variable to be displayed
+var A = A.fontcolor("blue");        //Color I wanted Variable in
+var B = "Sheeps have no souls!";    //Variable to be displayed
+var B = B.fontcolor("pink");        //Color I wanted Variable in
+
+var animal = (A) + (B)              //Concatenated the variables
+
+document.write (animal)             //Wrote out my Concatenate
+
+document.write(10*10);
