36593cd95fcc249b0a94b5e8d5e4d8c789df0857
reiatsu86 <reiatsu86@gmail.com>
2020-07-06 16:24:34 -0600

    add #5 of both to file

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,23 @@
+function not_function() {
+    document.getElementById("not").innerHTML = !(5>10);
+}
+
+document.write(2>1 && 4<2);
+
+document.write("<br>");
+
+document.write(2<3 || 1>2);
+
+document.write("<br>");
+
+x=1;
+y="2";
+document.write(x===y);
+
+document.write("<br>");
+
+document.write(typeof "Dog");
+
+document.write("<br>");
+
+document.write(1110+1 == 1111);
