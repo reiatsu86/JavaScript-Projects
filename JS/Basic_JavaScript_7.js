9b3042ea196fcef09d02f41bcf0e8ae3ed81ec72
reiatsu86 <reiatsu86@gmail.com>
2020-07-07 17:06:02 -0600

    Add next assignment

---
+++ JS/Basic_JavaScript_7.js
@@ -0,0 +1,26 @@
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 8 ==Time > 0) {
+        Reply = "It is time for some cereal!";
+    }
+    else if (Time > 8 == Time < 18) {
+        Reply = "It is time for a sandwich.";
+    }
+    else {
+        Reply = "it is time for a stake";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
+
+
+var A = 11;
+function Add_numbers_1() {
+    var B = 21;
+    console.log (11 + B);
+}
+function Add_numbers_2() {
+    console.log(B + A);
+}
+Add_numbers_1();
+Add_numbers_2();
